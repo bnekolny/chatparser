26198775e929d6d5fa3d77b8853e955e4708a257
Brett <nekolny.brett@gmail.com>
2024-06-28 15:07:20 +0200

    Add a resource for the prompt in comments

--- gemini.go
+++ gemini.go
@@ -7,6 +7,7 @@ import (
 	"os"
 )
 
+// https://cvc.cervantes.es/ensenanza/biblioteca_ele/marco/cvc_mer.pdf p43
 var promptPretext string = `
 The below text was written across WhatsApp message by someone who is learning a new language. I'll give you a bit more information about them and their journey, but what I'd like you to do is read the text, extremely breifly summarize how they're doing with the language, give an approximate level rating within the A1,A2,B1,B2,C1,C2 rating system as well as the amount of content you read to provide this rating, and finally, provide a suggestion of something to focus on related to the provided text (such as a type of repeat error, or a way to improve the gramatical abilities. Please give up to five examples if possible.
 
