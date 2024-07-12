a65906cdcb5b857d31a51f1446ddbda905d79440
Brett <nekolny.brett@gmail.com>
2024-07-12 17:10:54 +0200

    get ssl working locally, and ensure that the prodlike docker container can build.

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -2,18 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/static/',
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: 'http://chatparser:8080',
-        changeOrigin: true,
-        //rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    base: '/static/',
+    plugins: [react()],
+    server: {
+      https: {
+        key: '../.cert/key.pem',
+        cert: '../.cert/cert.pem',
       },
+      host: '0.0.0.0',
+      port: 443,
+      proxy: {
+        '/api': {
+          target: 'http://chatparser:8080',
+          changeOrigin: true,
+          //rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     }
   }
 })
