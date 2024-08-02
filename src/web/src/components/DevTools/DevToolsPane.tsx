0b9649e42a512380058b01de94d275a6fcb6acd2
Brett <nekolny.brett@gmail.com>
2024-08-02 14:52:42 +0200

    clean up UI a bit more so that it's more usable

--- src/web/src/components/DevTools/DevToolsPane.tsx
+++ src/web/src/components/DevTools/DevToolsPane.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TestTextButton from './TestTextButton';
 import TestPromptTextArea from './TestPromptTextArea';
 import styles from './styles/DevToolsPane.module.css';
 
 const DevToolsPane: React.FC = () => {
+  useEffect(() => {
+    document.body.classList.add(styles.devToolsPaneActive);
+    return () => {
+      document.body.classList.remove(styles.devToolsPaneActive);
+    };
+  }, []);
+
   return (
     <div className={styles.devToolsPane}>
       <TestTextButton />
