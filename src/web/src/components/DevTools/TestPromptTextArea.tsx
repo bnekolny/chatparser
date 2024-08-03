344eea3ac0830b815df7904372b91a72050ba386
Brett <nekolny.brett@gmail.com>
2024-08-04 01:28:48 +0200

    functionally complete.

--- src/web/src/components/DevTools/TestPromptTextArea.tsx
+++ src/web/src/components/DevTools/TestPromptTextArea.tsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useChatContext } from '../../context/ChatContext';
 import { Mode, Locale } from '../../types';
 import styles from './styles/TestPromptTextArea.module.css';
 import Button from '../Button';
+import {shortLanguageCode} from '../../utils';
 
 const TestPromptTextArea: React.FC = () => {
-  const { handleSendMessage, prompt, setPrompt, mode, setMode, locale, setLocale } = useChatContext();
+  const { handleSendMessage, prompt, setPrompt, mode, setMode } = useChatContext();
+  const { i18n } = useTranslation();
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(`/assets/prompts/${locale}/${mode}.txt`);
+      const response = await fetch(`/assets/prompts/${i18n.language}/${mode}.txt`);
       setPrompt(await response.text());
     })();
-  }, [locale, mode]);
+  }, [i18n.language, mode]);
 
   return (
     <div>
-      <select value={locale} onChange={(event) => setLocale(event.target.value as Locale)}>
+      <select value={shortLanguageCode(i18n.language)} onChange={(event) => i18n.changeLanguage(event.target.value as Locale)}>
         {Object.values(Locale).map((locale) => (
           <option key={locale} value={locale}>
             {locale}
